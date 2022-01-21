b84b08ab366880c5e9499ce44fce51ff28b48928
spences10 <spences10apps@gmail.com>
2022-01-21 18:21:04 +0000

    feat: :sparkles: get top sellers on inex

--- storefront/src/routes/index.svelte
+++ storefront/src/routes/index.svelte
@@ -1,5 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-  documentation
-</p>
+<script context="module">
+  import { client } from '$lib/graphql/graphql-client'
+  import { GET_TOP_SELLERS } from '$lib/graphql/queries'
+
+  export const load = async () => {
+    const {
+      search: { items },
+    } = await client.request(GET_TOP_SELLERS)
+
+    return {
+      props: {
+        items,
+      },
+    }
+  }
+</script>
+
+<script>
+  export let items
+</script>
+
+<div
+  class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52"
+>
+  {#each items as item}
+    <img
+      class=""
+      src={item.productAsset.preview}
+      alt={item.productName}
+    />
+    <p>{item.productName}</p>
+    <p>{item.priceWithTax.max}</p>
+  {/each}
+</div>
