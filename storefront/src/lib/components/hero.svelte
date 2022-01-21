e50db6d91e64b3c5b247fc31d512e9e6536e0f1b
spences10 <spences10apps@gmail.com>
2022-01-21 21:30:28 +0000

    feat: :construction: add hero component

---
+++ storefront/src/lib/components/hero.svelte
@@ -0,0 +1,7 @@
+<img
+  class="h-[35rem] w-full object-cover object-bottom"
+  src="/abel-y-costa-716024-unsplash__preview.jpg"
+  alt="table with pland on it"
+/>
+
+<h1 class="text-center">vendure storefront</h1>
--- storefront/src/routes/__layout.svelte
+++ storefront/src/routes/__layout.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Hero from '$lib/components/hero.svelte'
   import '../app.css'
 </script>
 
+<Hero />
 <main class="container max-w-6xl mx-auto px-4 mb-20">
   <slot />
 </main>
