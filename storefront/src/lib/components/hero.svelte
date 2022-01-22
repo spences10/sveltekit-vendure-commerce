81b702ec4c2eedb8ed15545f5ab798eb31e3495b
spences10 <spences10apps@gmail.com>
2022-01-22 13:01:43 +0000

    style: :lipstick: use daisyUI classes

--- storefront/src/lib/components/hero.svelte
+++ storefront/src/lib/components/hero.svelte
@@ -1,7 +1,11 @@
-<img
-  class="h-[35rem] w-full object-cover object-bottom"
-  src="/abel-y-costa-716024-unsplash__preview.jpg"
-  alt="table with pland on it"
-/>
-
-<h1 class="text-center">vendure storefront</h1>
+<div
+  class="hero min-h-[60vh] mb-10"
+  style="background-image: url(/abel-y-costa-716024-unsplash__preview.jpg);"
+>
+  <div class="hero-overlay bg-opacity-60" />
+  <div class="text-center text-neutral-content relative">
+    <h1 class="text-9xl font-bold tracking-widest opacity-80">
+      vendure storefront
+    </h1>
+  </div>
+</div>
