ec5d916db9e2561745b2d7cadd0574a2765ab9cc
spences10 <spences10apps@gmail.com>
2022-01-22 19:10:53 +0000

    feat: :sparkles: add category banner

---
+++ storefront/src/lib/components/category-banner.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { collectionsStore } from '../../stores/collections'
+
+  const items = $collectionsStore
+
+  const collections = items.filter(
+    item => item.parent.name === '__root_collection__'
+  )
+</script>
+
+<aside
+  class="grid gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 my-12"
+>
+  {#each collections as collection}
+    <a href={`/category/${collection.slug}`}>
+      <div class="card shadow-xl image-full">
+        <figure>
+          <img
+            src={collection.featuredAsset.preview}
+            alt={collection.name}
+          />
+        </figure>
+        <div class="justify-end card-body">
+          <h2 class="text-3xl font-bold tracking-widest">
+            {collection.name}
+          </h2>
+        </div>
+      </div>
+    </a>
+  {/each}
+</aside>
--- storefront/src/routes/index.svelte
+++ storefront/src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script context="module">
+  import CategoryBanner from '$lib/components/category-banner.svelte'
   import { client } from '$lib/graphql/graphql-client'
   import { GET_TOP_SELLERS } from '$lib/graphql/queries'
 
@@ -19,15 +20,19 @@
   export let items
 </script>
 
+<CategoryBanner />
+
+<h3 class="text-5xl text-neutral mb-8">Top Sellers</h3>
+
 <div
   class="grid gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
 >
   {#each items as item}
     <div>
       <div>
-        <a href={item.slug} class="">
+        <a href={`/product/${item.slug}`} class="">
           <img
-            class="object-cover"
+            class="object-cover rounded-2xl"
             src={`${item.productAsset.preview}?w=200&h=200`}
             alt={item.productName}
           />
