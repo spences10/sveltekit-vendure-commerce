2f291c31985b3327a712f9c712e4aec605bd90e8
spences10 <spences10apps@gmail.com>
2022-01-22 19:24:05 +0000

    feat: :sparkles: conditional render of hero

--- storefront/src/lib/components/hero.svelte
+++ storefront/src/lib/components/hero.svelte
@@ -1,11 +1,17 @@
-<div
-  class="hero min-h-[20vh] lg:min-h-[60vh] mb-10"
-  style="background-image: url(/abel-y-costa-716024-unsplash__preview.jpg);"
->
-  <div class="hero-overlay bg-opacity-40" />
-  <div class="text-center text-neutral-content relative">
-    <h1 class="text-5xl lg:text-9xl font-bold tracking-widest">
-      vendure storefront
-    </h1>
+<script>
+  export let key
+</script>
+
+{#if key === '/'}
+  <div
+    class="hero min-h-[20vh] lg:min-h-[60vh] mb-10"
+    style="background-image: url(/abel-y-costa-716024-unsplash__preview.jpg);"
+  >
+    <div class="hero-overlay bg-opacity-40" />
+    <div class="text-center text-neutral-content relative">
+      <h1 class="text-5xl lg:text-9xl font-bold tracking-widest">
+        vendure storefront
+      </h1>
+    </div>
   </div>
-</div>
+{/if}
--- storefront/src/routes/__layout.svelte
+++ storefront/src/routes/__layout.svelte
@@ -1,11 +1,21 @@
+<script context="module">
+  export const load = async ({ url }) => ({
+    props: {
+      key: url.pathname,
+    },
+  })
+</script>
+
 <script>
   import Hero from '$lib/components/hero.svelte'
   import Navbar from '$lib/components/navbar.svelte'
   import '../app.css'
+
+  export let key
 </script>
 
 <Navbar />
-<Hero />
+<Hero {key} />
 <main class="container max-w-6xl mx-auto px-4 mb-20">
   <slot />
 </main>
