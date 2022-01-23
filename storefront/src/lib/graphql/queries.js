03b539b45fd75cc884aa14a636ea6d6c0dd04435
spences10 <spences10apps@gmail.com>
2022-01-23 21:29:11 +0000

    feat: :sparkles: add search products query

--- storefront/src/lib/graphql/queries.js
+++ storefront/src/lib/graphql/queries.js
@@ -44,3 +44,42 @@ export const GET_COLLECTIONS = gql`
     }
   }
 `
+
+export const SEARCH_PRODUCTS = gql`
+  query SearchProducts($input: SearchInput!) {
+    search(input: $input) {
+      items {
+        productId
+        slug
+        productName
+        description
+        priceWithTax {
+          ... on PriceRange {
+            min
+            max
+          }
+        }
+        productAsset {
+          id
+          preview
+          focalPoint {
+            x
+            y
+          }
+        }
+      }
+      totalItems
+      facetValues {
+        count
+        facetValue {
+          id
+          name
+          facet {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+`
