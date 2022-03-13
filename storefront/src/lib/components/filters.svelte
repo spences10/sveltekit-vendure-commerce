bb65a56d0bc3bb3c12185447e9a69e39e2dea20a
spences10 <spences10apps@gmail.com>
2022-03-13 09:15:05 +0000

    refactor: :recycle: add types and name loop variables

--- storefront/src/lib/components/filters.svelte
+++ storefront/src/lib/components/filters.svelte
@@ -1,17 +1,29 @@
 <script lang="ts" context="module">
   import { KQL_SearchProducts } from '$lib/graphql/_kitql/graphqlStores'
+  import type { LoadOutput } from '@sveltejs/kit/types/private'
   import { filtersStore } from '../../stores/filters'
 
-  export const load = async ({ fetch }) => {
+  export const load = async ({ fetch }): Promise<LoadOutput> => {
     await KQL_SearchProducts.query({ fetch })
     return {}
   }
 </script>
 
 <script lang="ts">
+  export let facetValues: {
+    __typename?: 'FacetValueResult'
+    count: number
+    facetValue: {
+      __typename?: 'FacetValue'
+      id: string
+      name: string
+      facet: { __typename?: 'Facet'; id: string; name: string }
+    }
+  }[]
+  let filterValues = []
+
   $: facetValues = $KQL_SearchProducts?.data?.search?.facetValues
 
-  let filterValues = []
   $: filtersStore.set(filterValues)
 
   $: groupFacetValues = facetValues => {
@@ -44,23 +56,23 @@
 
 <div class="p-2 mr-6 w-1/5 h-full bordered card">
   <div class="form-control">
-    {#each facetValuesGrouped as item}
-      <p class="capitalize font-medium pl-1">{item.name}</p>
-      {#each item.values as item}
+    {#each facetValuesGrouped as { values, name }}
+      <p class="capitalize font-medium pl-1">{name}</p>
+      {#each values as valueItem}
         {@const name =
           [
             `Electronics`,
             `Home & Garden`,
             `Sports & Outdoor`, // TODO: make this dynamic
-          ].indexOf(item.name) > -1
+          ].indexOf(valueItem.name) > -1
             ? ``
-            : item.name}
+            : valueItem.name}
         {#if name.length > 0}
           <label class="cursor-pointer label py-1">
-            <span class="label-text">{item.name}</span>
+            <span class="label-text">{valueItem.name}</span>
             <input
               bind:group={filterValues}
-              value={item.id}
+              value={valueItem.id}
               type="checkbox"
               checked
               class="checkbox checkbox-sm checkbox-primary"
