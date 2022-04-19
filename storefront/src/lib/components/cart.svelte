b94f4ab560a53d65a01d8e0e85a3626bc4c40a87
spences10 <spences10apps@gmail.com>
2022-04-19 11:40:54 +0100

    feat: :sparkles: add basic cart component

---
+++ storefront/src/lib/components/cart.svelte
@@ -0,0 +1,11 @@
+<script context="module">
+  import { KQL_ActiveOrder } from '$lib/graphql/_kitql/graphqlStores'
+
+  export const load = async ({ fetch }) => {
+    await KQL_ActiveOrder.queryLoad({ fetch })
+    return {}
+  }
+</script>
+
+
+<pre>{JSON.stringify($KQL_ActiveOrder.data?.activeOrder, null, 2)}</pre>
--- storefront/src/routes/__layout.svelte
+++ storefront/src/routes/__layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts" context="module">
+  import Cart from '$lib/components/cart.svelte'
   import Footer from '$lib/components/footer.svelte'
   import Hero from '$lib/components/hero.svelte'
   import Navbar from '$lib/components/navbar.svelte'
   import PageTransition from '$lib/components/page-transition.svelte'
   import { KQL_GetCollections } from '$lib/graphql/_kitql/graphqlStores'
-  import type { LoadOutput } from '@sveltejs/kit/types/internal'
   import { onMount } from 'svelte'
   import '../app.css'
   import { userLocale } from '../stores/locale'
 
-  export const load = async ({ url }): Promise<LoadOutput> => {
+  export const load = async ({ url }) => {
     await KQL_GetCollections.queryLoad({ fetch })
     return { props: { key: url.pathname } }
   }
@@ -29,6 +29,7 @@
 </script>
 
 <PageTransition refresh={key}>
+  <Cart />
   <Navbar {collections} />
   <Hero {key} />
   <main class="container max-w-6xl mx-auto px-4 mb-20">
