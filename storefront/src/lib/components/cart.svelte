816b6d0c6209257797d1ab791170a1aa89b03b90
Michael Bromley <michael@michaelbromley.co.uk>
2022-04-19 22:27:03 +0200

    feat: Get cart data working

--- storefront/src/lib/components/cart.svelte
+++ storefront/src/lib/components/cart.svelte
@@ -1,12 +1,16 @@
-<script context="module">
+<script lang="ts" context="module">
   import { KQL_GetActiveOrder } from '$lib/graphql/_kitql/graphqlStores'
 
-  export const load = async ({ fetch }) => {
-    await KQL_GetActiveOrder.queryLoad({ fetch })
-    return {}
-  }
+  // export const load = async ({ fetch }) => {
+  //   await KQL_GetActiveOrder.queryLoad({ fetch })
+  //   return {}
+  // }
 </script>
 
+<script lang="ts">
+  import { browser } from '$app/env';
+  $: browser && KQL_GetActiveOrder.query();
+</script>
 <pre>
-  {JSON.stringify($KQL_GetActiveOrder.data?.activeOrder, null, 2)}
+  {JSON.stringify($KQL_GetActiveOrder.data, null, 2)}
 </pre>
