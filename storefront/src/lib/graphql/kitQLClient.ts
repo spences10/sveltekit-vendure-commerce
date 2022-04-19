b5c0c3ac6fbd8bef00c632bff20d16d57586613a
Michael Bromley <michael@michaelbromley.co.uk>
2022-04-19 20:56:02 +0200

    fix: Set client credentials to "include"
    This allows the client to send cookie data to the Vendure server.

--- storefront/src/lib/graphql/kitQLClient.ts
+++ storefront/src/lib/graphql/kitQLClient.ts
@@ -3,7 +3,7 @@ const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_API
 
 export const kitQLClient = new KitQLClient({
   url: GRAPHQL_ENDPOINT as string,
-  credentials: 'omit',
+  credentials: 'include',
   defaultCache: 1000 * 60 * 3,
   headersContentType: 'application/json',
   logType: ['client', 'server', 'operationAndvariables'],
