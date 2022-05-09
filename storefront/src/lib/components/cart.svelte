7bb65a0b4d2aa4762f3308895551fdc8861e724f
spences10 <spences10apps@gmail.com>
2022-05-09 21:02:10 +0100

    feat: :construction: add cart styles

--- storefront/src/lib/components/cart.svelte
+++ storefront/src/lib/components/cart.svelte
@@ -1,16 +1,27 @@
 <script lang="ts" context="module">
-  import { KQL_GetActiveOrder } from '$lib/graphql/_kitql/graphqlStores'
-
   // export const load = async ({ fetch }) => {
   //   await KQL_GetActiveOrder.queryLoad({ fetch })
   //   return {}
   // }
+  import { browser } from '$app/env'
+  import { KQL_GetActiveOrder } from '$lib/graphql/_kitql/graphqlStores'
+  import { fly } from 'svelte/transition'
 </script>
 
 <script lang="ts">
-  import { browser } from '$app/env';
-  $: browser && KQL_GetActiveOrder.query();
+  $: browser && KQL_GetActiveOrder.query()
+
+  export let isOpen: boolean = true
 </script>
-<pre>
-  {JSON.stringify($KQL_GetActiveOrder.data, null, 2)}
-</pre>
+
+{#if isOpen}
+  <section
+    in:fly={{ duration: 5000 }}
+    out:fly
+    class="top-0 right-0 fixed bg-primary h-full w-3/12 z-40"
+  >
+    <pre>
+    {JSON.stringify($KQL_GetActiveOrder.data, null, 2)}
+  </pre>
+  </section>
+{/if}
