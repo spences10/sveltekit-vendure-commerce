f369025a8df655f9edb6bec3ec88678d47ebb442
spences10 <spences10apps@gmail.com>
2022-05-10 10:38:37 +0100

    style: :lipstick: basic styles for car items

--- storefront/src/lib/components/cart.svelte
+++ storefront/src/lib/components/cart.svelte
@@ -12,6 +12,9 @@
   $: browser && KQL_GetActiveOrder.query()
 
   export let isOpen: boolean = true
+
+  let activeOrderLines =
+    $KQL_GetActiveOrder?.data?.activeOrder?.lines || []
 </script>
 
 <button on:click={() => (isOpen = !isOpen)}>Click {isOpen}</button>
@@ -20,10 +23,40 @@
   <section
     in:fly={{ x: 200, duration: 350 }}
     out:fly={{ x: 400, duration: 350 }}
-    class="top-0 right-0 fixed bg-primary h-full w-3/12 z-40"
+    class="p-10 top-0 right-0 fixed bg-base-100 shadow-xl h-full w-3/12 z-40"
   >
-    <pre>
-    {JSON.stringify($KQL_GetActiveOrder.data, null, 2)}
-  </pre>
+    <div class="flex justify-between align-middle text-3xl">
+      <button on:click={() => (isOpen = !isOpen)} class="block">
+        &#10799;
+      </button>
+      <p>Cart</p>
+    </div>
+    {#if !activeOrderLines.length}
+      <p class="text-2xl text-base-300">Cart is empty</p>
+    {:else}
+      {#each activeOrderLines as item}
+        <div class="my-6 flex ">
+          <div class="">
+            <img
+              src={`${item.featuredAsset.preview}?w=300&h=300`}
+              alt={item.productVariant.name}
+              class="object-cover rounded-lg h-16 w-16"
+            />
+          </div>
+          <div class="flex flex-col flex-grow text-lg pl-3">
+            <p class="">{item.productVariant.name}</p>
+            <div class="flex justify-between align-middle">
+              <p>price</p>
+              <p>plus minus</p>
+              <p>Total</p>
+            </div>
+          </div>
+        </div>
+      {/each}
+    {/if}
   </section>
 {/if}
+
+<!-- <pre class="flex flex-col flex-grow">
+  {JSON.stringify($KQL_GetActiveOrder.data, null, 2)}
+</pre> -->
